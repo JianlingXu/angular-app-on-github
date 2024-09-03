3f09c75f38b62b9e71bc5b59b5b0b5056f39d554
Jianling Xu <xu_jianling@hotmail.com>
2024-09-03 07:53:53 -0400

    routers work

--- buy-and-sell/src/app/app.component.ts
+++ buy-and-sell/src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet],
-  standalone: true,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
